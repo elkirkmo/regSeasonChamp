867f7bf41aabd44cfa8139cbb7bd1359ca2afdc0
Chris Kirkham <ckirkham@fandango.com>
2017-03-20 17:58:43 -0600

    rearranging folder structure, adding new dependencies

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var rootPath = path.normalize(__dirname + '/../');
 
-app.use(express.static(rootPath + '/app'));
+app.get('/*', function (req, res){
+   res.sendFile(__dirname + '/nba.html') 
+});
 
 app.listen(3000);
 console.log('port 3000 live');
