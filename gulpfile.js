1da80cd3d15002852901800a16e72fb3b7605544
Chris Kirkham <ckirkham@fandango.com>
2017-03-20 21:07:12 -0600

    boom

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ var path = require('path');
 var plumber = require('gulp-plumber');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('watch', function(){
     gulp.watch('./styles/less/*.less', ['less']);
-})
+});
 
 gulp.task('less', function(){
     gulp.src('./styles/less/*.less')
