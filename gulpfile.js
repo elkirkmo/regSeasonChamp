b71171d3532055d8719ef062fb1f6546b570077f
Chris Kirkham <ckirkham@fandango.com>
2017-03-20 18:25:36 -0600

    switched to less

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var plumber = require('gulp-plumber');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+
+gulp.task('watch', function(){
+    gulp.watch('./styles/less/*.less', ['less']);
+})
+
+gulp.task('less', function(){
+    gulp.src('./styles/less/*.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(gulp.dest('./styles/css/'))
+    .pipe(cssmin())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('./styles/css/'))
+})
